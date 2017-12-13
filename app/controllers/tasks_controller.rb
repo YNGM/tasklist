6bba024ba6cbc6929f685ca58a03f365ef951745
YNGM <you_nagami@hotmail.com>
2017-12-13 14:57:55 +0000

    CRUD messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,18 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(patams[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Tasks.new(task_params)
   end
 
   def edit
